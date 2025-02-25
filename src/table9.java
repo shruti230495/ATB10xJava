a2b5e47fbad14970816bbc47373a27d6533a15dc
shruti230495 <shrutib2395@gmail.com>
2025-02-25 23:39:30 +0530

    my 3rd lab commit

---
+++ src/table9.java
@@ -0,0 +1,25 @@
+public class table9 {
+    public static void main (String[] args){
+        int a = 9;
+                System.out.printf("%d",a*1);
+                System.out.println();
+        System.out.printf("%d",a*2);
+        System.out.println();
+        System.out.printf("%d",a*3);
+        System.out.println();
+        System.out.printf("%d",a*4);
+        System.out.println();
+        System.out.printf("%d",a*5);
+        System.out.println();
+        System.out.printf("%d",a*6);
+        System.out.println();
+        System.out.printf("%d",a*7);
+        System.out.println();
+        System.out.printf("%d",a*8);
+        System.out.println();
+        System.out.printf("%d",a*9);
+        System.out.println();
+        System.out.printf("%d",a*10);
+        System.out.println();
+    }
+}
