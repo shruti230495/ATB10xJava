078e730e8f381c98b211eed0d76bddb4a2dcb0a3
shruti230495 <shrutib2395@gmail.com>
2025-02-25 10:37:22 +0530

    my 1st lab commit

---
+++ src/lab001.java
@@ -0,0 +1,15 @@
+
+
+public class lab001 {
+    public static void main (String[] args){
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(3/2);
+
+        //println prints integer part not decimal
+    }
+
+}
