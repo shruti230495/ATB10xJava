2d363c10f1940a0f2bd2f973470ed298f08536e9
shruti230495 <shrutib2395@gmail.com>
2025-02-27 00:12:00 +0530

    my 4rd lab commit

---
+++ java class/src/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+
+    public static void main(String[] args){
+
+        //Arithmatic operators
+
+        int a=20,b=10;
+        System.out.println("Addition of a and b is:"+(a+b));
+        System.out.println("Subtraction of a and b is:"+(a-b));
+        System.out.println("Multiplication of a and b is:"+(a*b));
+        System.out.println("Division of a and b is:"+(a/b));
+        System.out.println("Modulus of a and b is:"+(a%b));
+
+        //Relational or comparision operators
+        System.out.println(a>b);   //true
+        System.out.println(a<b);   //false
+        System.out.println(a>=b);  //true
+        System.out.println(a<=b);  //false
+        System.out.println(a!=b);  //true
+
+    }
+}
---
+++ src/swapping_of_numbers.java
@@ -0,0 +1,16 @@
+public class swapping_of_numbers {
+    public static void main (String[] args){
+
+        int a = 10;
+        int b = 20;
+        //before swapping
+        System.out.println("The value of a is:"+a);
+        System.out.println("The value of b is:"+b);
+        int c = a;
+        a = b;
+        b = c;
+        //after swapping
+        System.out.println("The value of a is:"+a);
+        System.out.println("The value of b is:"+b);
+    }
+}
