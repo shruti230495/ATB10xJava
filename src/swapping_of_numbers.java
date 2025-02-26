dfa154feb1225c67dc7d618ea0cf69a6f52caf11
shruti230495 <shrutib2395@gmail.com>
2025-02-27 00:46:56 +0530

    my 4rd lab commit

--- src/swapping_of_numbers.java
+++ src/swapping_of_numbers.java
@@ -1,6 +1,6 @@
 public class swapping_of_numbers {
     public static void main (String[] args){
-
+        //swapping of number using 3rd variable
         int a = 10;
         int b = 20;
         //before swapping
@@ -12,5 +12,6 @@ public class swapping_of_numbers {
         //after swapping
         System.out.println("The value of a is:"+a);
         System.out.println("The value of b is:"+b);
+
     }
 }
