8732ed00fe4521850c921f1925f2a79e6bcd061a
shruti230495 <shrutib2395@gmail.com>
2025-02-27 00:46:56 +0530

    my 4rd lab commit

---
+++ src/swap_number_arithmatic_operator.java
@@ -0,0 +1,13 @@
+public class swap_number_arithmatic_operator {
+    public static void main(String[] args){
+
+        //swap with arithmatic operators
+        int a= 10,b = 20;
+        a = (a + b);
+        b = (a - b);
+        a = (a - b);
+        System.out.println("the value of a is:"+a);
+        System.out.println("the value of b is:"+b);;
+
+    }
+}
