f225f4c16e12740e9d9cca4828728a3f1b1808fc
shruti230495 <shrutib2395@gmail.com>
2025-03-07 10:01:30 +0530

    My 6th commit

---
+++ src/lab006evenodd_ifelse.java
@@ -0,0 +1,13 @@
+public class lab006evenodd_ifelse {
+    public static void main(String[] args){
+        int number= 5;
+        if(number % 2 == 0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is odd");
+        }
+    }
+}
