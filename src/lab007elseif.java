60948c43a66ae1d0ea61f955d60b08687bded76d
shruti230495 <shrutib2395@gmail.com>
2025-03-07 10:11:06 +0530

    My 7th Commit

---
+++ src/lab007elseif.java
@@ -0,0 +1,17 @@
+public class lab007elseif {
+    public static void main(String[] args){
+        int number= 5;
+        if(number>0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if(number<0)
+        {
+           System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is 0");
+        }
+    }
+}
