1b26a1a38b94ea30e4ce06b0e450600d51c59670
shruti230495 <shrutib2395@gmail.com>
2025-03-07 10:19:07 +0530

    My 8th Commit

---
+++ src/lab008largest_number.java
@@ -0,0 +1,17 @@
+public class lab008largest_number {
+    public static void main(String[] args){
+        int a=34,b=56,c=67;
+        if(a>b && a>c)
+        {
+            System.out.println("a is the largest number");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println("b is the largest number");
+        }
+        else if(c>a && c>b)
+        {
+            System.out.println("c is the largest number");
+        }
+    }
+}
