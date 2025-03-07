8b5066493357a1b49035c3cb7bc798409a541d36
shruti230495 <shrutib2395@gmail.com>
2025-03-07 09:55:53 +0530

    My 5th commit

---
+++ src/lab005ifelse.java
@@ -0,0 +1,13 @@
+public class lab005ifelse {
+    public static void main(String[] args){
+        int person_age= 17;
+        if(person_age >= 18)
+        {
+            System.out.println("Person is eligibled for voting");
+        }
+        else
+        {
+          System.out.println("Person is not eligible for voting");
+        }
+    }
+}
