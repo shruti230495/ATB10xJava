92098bd437de03f761b247ccb987bbf3e7e27a5a
shruti230495 <shrutib2395@gmail.com>
2025-03-08 10:01:40 +0530

    My 12th Commit

---
+++ src/lab012_printhello10_while.java
@@ -0,0 +1,10 @@
+public class lab012_printhello10_while {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println("Hello");
+            i++;
+        }
+    }
+}
