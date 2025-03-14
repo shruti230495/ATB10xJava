7d81935be3c8219f765fc281961adb7377ca60a2
shruti230495 <shrutib2395@gmail.com>
2025-03-14 23:37:30 +0530

    My 23nd commit

---
+++ src/lab023_reversearray.java
@@ -0,0 +1,11 @@
+public class lab023_reversearray {
+    public static void main(String[] args){
+        int a[] ={10,20,30,40,50};
+        for(int i=4;i>=0;i--)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
